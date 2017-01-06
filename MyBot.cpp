f81f230d4d23b0386cc5c657553cc01bfb386fb2
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-05 20:45:11 -0800

    Corrected some logic

--- MyBot.cpp
+++ MyBot.cpp
@@ -12,23 +12,6 @@
 #include "networking.hpp"
 using namespace std;
 
-using US=unsigned short;
-using UC=unsigned char;
-typedef pair<US, US> PII;
-
-int enemystrength(hlt::GameMap& presentMap, UC myID, US a, US b) {
-    vector<PII> dirs = {{0,0},{-1,0},{0,1},{1,0},{0,-1}};
-    int ret = 0;
-    for (PII dir : dirs) {
-        US nb = b + dir.second, na = a + dir.first;
-        auto psite = presentMap.getSite({ nb, na });
-        if (psite.owner == myID) continue;
-        ret += psite.strength;
-    }
-    return ret;
-}
-
-
 int main() {
     srand(time(NULL));
 
@@ -41,75 +24,98 @@ int main() {
     sendInit("yckuoBot");
 
     std::set<hlt::Move> moves;
+    vector<vector<bool>> source, target;
     while(true) {
         moves.clear();
 
         getFrame(presentMap);
         
-        for(US a = 0; a < presentMap.height; a++) {
-            for(US b = 0; b < presentMap.width; b++) {
+        source.resize(presentMap.height, vector<bool>(presentMap.width, false));
+        target.resize(presentMap.height, vector<bool>(presentMap.width, false));
+
+        for(unsigned short a = 0; a < presentMap.height; a++) {
+            for(unsigned short b = 0; b < presentMap.width; b++) {
                 auto site = presentMap.getSite({ b, a });
-                if (site.owner == myID) {
-                    // moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
+                if (site.owner != myID) continue;
                     
-                    hlt::Location loc = { b, a };
+                hlt::Location loc = { b, a };
 
-                    vector<int> dangers(5, 0), profits(5, 0), mindangers(5, INT_MAX), strengths(5, 0);
-                    vector<bool> isMe(5, false);
+                vector<int> dangers(5, 0), profits(5, 0), mindangers(5, INT_MAX), strengths(5, 0);
+                vector<bool> isMe(5, false);
 
-                    for (int D : CARDINALS) {
-                        hlt::Location nloc = presentMap.getLocation(loc, D);
-                        hlt::Site nsite = presentMap.getSite(nloc);
-                        isMe[D] = nsite.owner == myID;
-                        strengths[D] = nsite.strength;
-                        // if (nsite.owner == myID) continue;
+                for (int D : CARDINALS) {
+                    hlt::Location nloc = presentMap.getLocation(loc, D);
+                    hlt::Site nsite = presentMap.getSite(nloc);
+                    isMe[D] = nsite.owner == myID;
+                    strengths[D] = nsite.strength;
+                    profits[D] = nsite.production;
 
-                        profits[D] = nsite.production;
-                        for (int D2 : DIRECTIONS) {
-                            hlt::Site esite = presentMap.getSite(nloc, D2);
-                            if (esite.owner != myID) {
-                                dangers[D] += esite.strength;
-                                mindangers[D] = min(mindangers[D], (int)esite.strength);
-                            }
+                    if (isMe[D]) {
+                        dangers[D] = nsite.strength;
+                        mindangers[D] = min(mindangers[D], (int)nsite.strength);
+                    }
+                    for (int D2 : DIRECTIONS) {
+                        hlt::Site esite = presentMap.getSite(nloc, D2);
+                        if (esite.owner != myID) {
+                            dangers[D] += esite.strength;
+                            mindangers[D] = min(mindangers[D], (int)esite.strength);
                         }
                     }
-
-                    int bestD = STILL, bestProfit = -1;
-                    for (int D : CARDINALS) {
-                        if (!isMe[D] && dangers[D] == 0 && profits[D] > bestProfit) {
-                            bestProfit = profits[D];
-                            bestD = D;
-                        }
-                    }
-
-                    if (bestD != STILL) {
-                        moves.insert({ {b, a}, (unsigned char)bestD });
-                        continue;
-                    }
-
-                    for (int D : CARDINALS) {
-                        if (!isMe[D] && dangers[D] < site.strength) {
-                            bestD = D;
-                            break;
-                        }
-                    }
-
-                    if (bestD != STILL) {
-                        moves.insert({ {b, a}, (unsigned char)bestD });
-                        continue;
-                    }
-
-                    for (int D : CARDINALS) {
-                        int totalStrength = site.strength;
-                        if (isMe[D]) totalStrength += strengths[D];
-                        if (mindangers[D] <= totalStrength) {
-                            bestD = D;
-                            break;
-                        }
-                    }
-
-                    moves.insert({ {b, a}, (unsigned char)bestD });
                 }
+
+                // first, find the neighbor site not owned by me, with the largest profit (could be 0), and without enemies that can kill me.
+                int bestD = STILL, bestProfit = -1;
+                for (int D : CARDINALS) {
+                    if (!isMe[D] && dangers[D] < site.strength && profits[D] > bestProfit) {
+                        bestProfit = profits[D];
+                        bestD = D;
+                    }
+                }
+
+                if (bestD != STILL) {
+                    moves.insert({ {b, a}, (unsigned char)bestD });
+                    continue;
+                }
+
+                // Next, try a neighbor site not owned by me, where we can kill someone, with the largest profit.
+                bestProfit = -1;
+                for (int D : CARDINALS) {
+                    if (isMe[D]) continue;
+                    if (mindangers[D] <= site.strength && profits[D] > bestProfit) {
+                        bestProfit = profits[D];
+                        bestD = D;
+                    }
+                }
+
+                if (bestD != STILL) {
+                    moves.insert({ {b, a}, (unsigned char)bestD });
+                    continue;
+                }
+
+                // Next, try a neighbor site owned by me, where we can kill someone. In order to prevent two pieces from
+                // swapping locations and ending up killing nobody, we try to detect duplicate moves. If we are to move A
+                // to B, and if we see another attempt of someone else moving into A or moving out of B, then we simply reject it.
+                // First come first serve, not ideal but will improve this in V3.
+                for (int D : CARDINALS) {
+                    if (!isMe[D]) continue;
+                    int totalStrength = site.strength + strengths[D];
+                    if (mindangers[D] <= totalStrength && mindangers[D] > 0) {
+                        bestD = D;
+                    }
+                }
+
+                if (bestD != STILL) {
+                    hlt::Location bestLoc = presentMap.getLocation(loc, bestD);
+                    // attempt loc to bestLoc
+                    if (target[loc.y][loc.x] || source[bestLoc.y][bestLoc.x] || target[bestLoc.y][bestLoc.x]) {
+                        bestD = STILL;
+                    } else {
+                        target[bestLoc.y][bestLoc.x] = true;
+                        source[loc.y][loc.x] = true;
+                    }
+                }
+
+                moves.insert({ {b, a}, (unsigned char)bestD });
             }
         }
 
