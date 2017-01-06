a9cbf8b40e614b19b07302dca295dd805dd8cb1a
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-06 01:40:42 -0800

    bot v4

--- MyBot.cpp
+++ MyBot.cpp
@@ -3,8 +3,6 @@
 #include <cstdlib>
 #include <ctime>
 #include <time.h>
-#include <set>
-#include <unordered_set>
 #include <fstream>
 #include <vector>
 #include <algorithm>
@@ -12,33 +10,15 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 using namespace std;
-
-/*
-class Hasher {
-public:
-    size_t operator() (hlt::Location const& loc) const {
-        return loc.x * 10000 + loc.y;
-    }
-};
-*/
-class Comparer {
-public:
-    bool operator() (hlt::Location const& loc1, hlt::Location const& loc2) const {
-        if (loc1.x != loc2.x) return loc1.x < loc2.x;
-        return loc1.y < loc2.y;
-    }
-};
+using namespace hlt;
 
 int main() { 
     srand(time(NULL));
 
-    vector<vector<int>> orders = {
-        {1, 2, 3, 4},{1, 2, 4, 3},{1, 3, 2, 4},{1, 3, 4, 2},{1, 4, 2, 3},{1, 4, 3, 2},
-        {2, 1, 3, 4},{2, 1, 4, 3},{2, 3, 1, 4},{2, 3, 4, 1},{2, 4, 1, 3},{2, 4, 3, 1},
-        {3, 1, 2, 4},{3, 1, 4, 2},{3, 2, 1, 4},{3, 2, 4, 1},{3, 4, 1, 2},{3, 4, 2, 1},
-        {4, 1, 2, 3},{4, 1, 3, 2},{4, 2, 1, 3},{4, 2, 3, 1},{4, 3, 1, 2},{4, 3, 2, 1}};
-
     std::cout.sync_with_stdio(0);
+
+    ofstream log;
+    log.open("log.txt");
 
     unsigned char myID;
     hlt::GameMap presentMap;
@@ -47,15 +27,26 @@ int main() {
     sendInit("yckuoBot");
 
     std::set<hlt::Move> moves;
+    double pi = 3.1415926;
     while(true) {
         moves.clear();
 
         getFrame(presentMap);
 
-        // unordered_set<hlt::Location, Comparer> visited;
-        set<hlt::Location> visited;
-
-        // unordered_map<hlt::Location, unsigned char, Hasher, Comparer> attempts;
+        double ca = 0, cb = 0, sumw = 0;
+        for (unsigned short a = 0; a < presentMap.height; a++) {
+            for (unsigned short b = 0; b < presentMap.width; b++) {
+                auto site = presentMap.getSite({ b, a});
+                if (site.owner != myID) continue;
+                ca += a * site.strength;
+                cb += b * site.strength;
+                sumw += site.strength;
+            }
+        }
+        ca /= sumw;
+        cb /= sumw;
+        Location center = { (unsigned short)cb, (unsigned short)ca };
+        log << "center " << center.x << ", " << center.y << endl;
         
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
@@ -118,75 +109,52 @@ int main() {
                     continue;
                 }
 
-                // Finally, between 2 sites owned by me, we want to flow from higher strengths to lower strengths.
-                // We will allow A -> B flow if:
-                // 1) both A and B are safe zones,
-                // 2) A - B > 50, and 
-                // 3) total neighbor strengths except A and B themselves: A > B
+                
+                
                 if (dangers[STILL] > 0) {
-                    moves.insert({ {b, a}, STILL });
+                    // TODO: if production is 0, might as well move towards danger to kill something
+                    // moves.insert({ {b, a}, STILL });
                     continue;
                 }
 
-                int bestgap = 0;
-                int randidx = rand() % 24;
-                for (int D : orders[randidx]) {
-                // for (int D : CARDINALS) {
-                    if (!isMe[D] || dangers[D] > 0) continue;
-                    if (strengths[D] >= site.strength || site.strength - strengths[D] <= 80) continue;
-                    if (powers[D] - (int)site.strength >= powers[STILL] - strengths[D]) continue;
-                    int gap = powers[STILL] - strengths[D] - powers[D] + site.strength;
-                    if (bestgap < gap) {
-                        bestD = D;
-                        bestgap = gap;
-                    }
-                }
+                float angle = presentMap.getAngle(center, loc);
+                unsigned char D = STILL;
 
-                if (bestD == STILL) {
-                    moves.insert({ {b, a}, STILL });
-                    continue;
-                }
-
-                hlt::Location bestloc = presentMap.getLocation(loc, bestD);
-                if (visited.count(bestloc)) {
-                    moves.insert({ {b, a}, STILL });
+                vector<int> options;
+                if (angle > 0 && angle < 0.5 * pi) {
+                    options = {NORTH, EAST};
+                } else if (angle > 0.5 * pi) {
+                    options = {NORTH, WEST};
+                } else if (angle < 0 && angle > -0.5 * pi) {
+                    options = {SOUTH, EAST};
+                } else if (angle < -0.5) {
+                    options = {SOUTH, WEST};
                 } else {
-                    visited.insert(bestloc);
-                    moves.insert({ {b, a}, (unsigned char)bestD });
+                    continue;
                 }
 
+                D = options[rand()%2];
 
-                /*
+                if (!isMe[D]) continue;
 
-                // Next, try a neighbor site owned by me, where we can kill someone. Minimum efforts to prevent swapping
-                // positions.
-                for (int D : CARDINALS) {
-                    if (!isMe[D]) continue;
-                    int totalStrength = site.strength + strengths[D];
-                    if (mindangers[D] <= totalStrength && mindangers[D] > 0) {
-                        bestD = D;
-                    }
-                }
+                // TODO: powers not needed anymore?
+                Location outloc = presentMap.getLocation(loc, D);
+                Site outsite = presentMap.getSite(outloc);
+                if (strengths[D] >= site.strength || site.strength - strengths[D] <= 30) continue;
 
-                if (bestD != STILL) {
-                    attempts[loc] = bestD;
-                }
-                */
+                if (D == STILL) continue;
+                moves.insert({ {b, a}, D });
+
 
                 // moves.insert({ {b, a}, (unsigned char)bestD });
             }
         }
-
-        /*
-        for (auto it : attempts) {
-            hlt::Location target = presentMap.getLocation(it.first, it.second);
-            if (!attempts.count(target)) {
-                //moves.insert({ {it.first, it.second} });
-            }
-        }*/
+        log << endl;
 
         sendFrame(moves);
     }
+
+    log.close();
 
     return 0;
 }
