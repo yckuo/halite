aaf859ff009dcc28bae74ffdbc113260edbbd895
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2017-01-07 15:34:45 -0800

    Boundary pixels counting - add momentum - still oscillating and inefficent

--- MyBot.cpp
+++ MyBot.cpp
@@ -7,12 +7,23 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <unordered_set>
 #include <unordered_map>
 #include <queue>
 #include "hlt.hpp"
 #include "networking.hpp"
 using namespace std;
 using namespace hlt;
+
+const double pi = 3.1415926;
+
+unsigned char angle2Direction(float angle) {
+    if (angle >= 0.25 * pi && angle <= 0.75 * pi) return NORTH;
+    if (angle > 0.75 * pi || angle < -0.75 * pi) return WEST;
+    if (angle <= -0.25 * pi && angle >= -0.75 * pi) return SOUTH;
+    if (angle > -0.25 * pi && angle < 0.25 * pi) return EAST;
+    return STILL;
+}
 
 int main() { 
     srand(time(NULL));
@@ -28,18 +39,44 @@ int main() {
 
     sendInit("yckuoBot");
 
+    unordered_map<Location, Location, LocationHasher, LocationComparer> sources, targets, presources, pretargets;
+
     std::set<hlt::Move> moves;
-    double pi = 3.1415926;
     while(true) {
+        presources = sources;
+        pretargets = targets;
+        sources.clear();
+        targets.clear();
+
         moves.clear();
 
         getFrame(presentMap);
+
+        unordered_set<Location, LocationHasher, LocationComparer> borders, enemies;
+        for (unsigned short a = 0; a < presentMap.height; a++) {
+            for (unsigned short b = 0; b < presentMap.width; b++) {
+                Location loc = { b, a };
+                Site site = presentMap.getSite(loc);
+                if (site.owner == myID) continue;
+                int count = 0;
+                for (unsigned char D : CARDINALS) {
+                    Site nsite = presentMap.getSite(loc, D);
+                    if (nsite.owner == myID) {
+                        if (++count >= 2) {
+                            borders.insert(loc);
+                            enemies.insert(loc);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
                 auto site = presentMap.getSite({ b, a });
                 if (site.owner != myID) continue;
-                    
+    
                 Location loc = { b, a };
 
                 vector<int> dangers(5, 0), profits(5, 0), mindangers(5, INT_MAX), strengths(5, 0);
@@ -53,14 +90,14 @@ int main() {
                     profits[D] = nsite.production; // TODO: consider potential future profit (surrounding enemy tiles)
 
                     if (!isMe[D]) {
-                        dangers[D] = nsite.strength;
-                        mindangers[D] = min(mindangers[D], (int)nsite.strength);
+                        dangers[D] = nsite.strength + nsite.production;
+                        mindangers[D] = min(mindangers[D], (int)nsite.strength + nsite.production);
                     }
-                    for (int D2 : DIRECTIONS) {
+                    for (int D2 : CARDINALS) {
                         Site esite = presentMap.getSite(nloc, D2);
                         if (esite.owner != myID) {
-                            dangers[D] += esite.strength;
-                            mindangers[D] = min(mindangers[D], (int)esite.strength);
+                            dangers[D] += esite.strength + esite.production;
+                            mindangers[D] = min(mindangers[D], (int)esite.strength + esite.production);
                         }
                     }
                 }
@@ -75,7 +112,8 @@ int main() {
                 }
 
                 if (bestD != STILL) {
-                    moves.insert({ {b, a}, (unsigned char)bestD });
+                    moves.insert({ loc, (unsigned char)bestD });
+                    enemies.erase(presentMap.getLocation(loc, bestD));
                     continue;
                 }
 
@@ -90,11 +128,12 @@ int main() {
 
                 if (bestD != STILL) {
                     moves.insert({ {b, a}, (unsigned char)bestD });
+                    enemies.erase(presentMap.getLocation(loc, bestD));
                     continue;
                 }
 
                 // If there is enemy near by, and we get some production where we are, why not just stay and wait for growth.
-                if (dangers[STILL] > 0 && profits[STILL] > 0) {
+                if (dangers[STILL] > 0 /*&& profits[STILL] > 0*/) {
                     continue;
                 }
 
@@ -102,36 +141,50 @@ int main() {
                     continue;
                 }
 
-                bestD = STILL;
-                int bestDist = INT_MAX;
-                bestProfit = -1;
-                for (int D : CARDINALS) {
-                    Location nloc = loc;
-                    Site nsite = presentMap.getSite(nloc);
-                    int dist = 0;
-                    while (nsite.owner == myID && dist < presentMap.width) {
-                        nloc = presentMap.getLocation(nloc, D);
-                        nsite = presentMap.getSite(nloc);
-                        dist++;
-                    }
+                float bestDist = 10000;
+                for (Location border : borders) {
+                    float dist = presentMap.getDistance(loc, border);
+                    bestDist = min(bestDist, dist);
+                }
 
-                    if (nsite.owner == myID) continue;
-                    if (dist < bestDist || (dist == bestDist && (nsite.production > bestProfit || (nsite.production == bestProfit && (D == NORTH || D == WEST))))) {
-                        bestDist = dist;
-                        bestProfit = nsite.production;
-                        bestD = D;
+                vector<int> dcounts(5, 0);
+                for (Location border : borders) {
+                    float dist = presentMap.getDistance(loc, border);
+                    if (bestDist == dist) {
+                        float angle = presentMap.getAngle(loc, border);
+                        unsigned char D = angle2Direction(angle);
+                        dcounts[D]++;
                     }
                 }
+
+
+                Location source = presources[loc];
+                bestD = STILL;
+                for (int D : CARDINALS) {
+                    // add momentum
+                    if (presentMap.getLocation(source, D) == loc) {
+                        dcounts[D] ++;
+                    }
+                    if (dcounts[D] > dcounts[bestD]) bestD = D;
+                }
+
+                if (bestD == STILL) continue;
 
                 Location outloc = presentMap.getLocation(loc, (unsigned char)bestD);
+
                 Site outsite = presentMap.getSite(outloc);
-                if (outsite.owner != myID) {
+                /*if (outsite.owner != myID) {
                     moves.insert({ {b, a}, (unsigned char)bestD });
                     continue;
-                }
+                }*/
+                if (site.strength <= strengths[bestD]) continue;
 
-                if (site.strength > strengths[bestD] && site.strength - strengths[bestD] > 50 || site.strength == 255 && strengths[bestD] != 255) {
-                    moves.insert({ {b, a}, (unsigned char)bestD });
+                int diff = site.strength - strengths[bestD], sum = (int)site.strength + strengths[bestD];
+                if (diff > 30 || (site.strength == 255  && strengths[bestD] != 255)) {
+                    moves.insert({ loc, (unsigned char)bestD });
+                    Location target = presentMap.getLocation(loc, bestD);
+                    sources[target] = loc;
+                    targets[loc] = target;
                 }
             }
         }
